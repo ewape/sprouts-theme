ba69fa470a90aeea4f4588c28325bf5ddebaf690
Ewa <ewapettke@gmail.com>
2016-12-19 19:47:40 +0100

    add search term quotation

--- lib/titles.php
+++ lib/titles.php
@@ -19,7 +19,7 @@ function title() {
   elseif (is_archive()) {
     return get_the_archive_title();
   } elseif (is_search()) {
-    return sprintf(__('Search Results for %s', 'sage'), get_search_query());
+    return sprintf(__('Search Results for &bdquo;%s&rdquo;', 'sage'), get_search_query());
   } elseif (is_404()) {
     return __('Not Found', 'sage');
   } else {
