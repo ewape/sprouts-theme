4dda160d14a64d47e58633cbb1d2a10d01e962e0
Ewa <ewapettke@gmail.com>
2016-12-30 18:29:20 +0100

    update partners widget

--- templates/widget-partners.php
+++ templates/widget-partners.php
@@ -2,10 +2,15 @@
 	<h2 class="widget-title">Programy partnerskie</h2>
 	<div class="widget-body">
 		<a class="widget-img-link" target="_blank" href="http://www.pp.selkar.pl/aff/kielki" title="">
-			<img src="<?php echo get_template_directory_uri(); ?>/dist/images/selkar.png" alt="Selkar">
+			<img src="<?php echo get_template_directory_uri(); ?>/dist/images/selkar.gif" alt="Selkar">
+			<span class="btn btn-accent-dark hvr-icon-pulse arrow">dołącz</span>
 		</a>
 		<a class="widget-img-link" target="_blank" href="http://www.zlotemysli.pl/kielki,1/" title="">
 			<img src="<?php echo get_template_directory_uri(); ?>/dist/images/zlotemysli-logo.png" alt="Złote myśli">
+			<span class="btn btn-accent-dark hvr-icon-pulse arrow">dołącz</span>
+		</a>
+		<a class="widget-img-link" href="https://partnerzy.ceneo.pl/Account/Register?referrer=13274" target="_blank"><img src="https://image2.ceneo.pl/data/banners/banner_rec_3.png" class="img-responsive" />
+			<span class="btn btn-accent-dark hvr-icon-pulse arrow">dołącz</span>
 		</a>
 	</div>
 </div>
