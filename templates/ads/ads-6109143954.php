a2ac2680f92ec9f0cb9e7ecabc709bc44ed37454
Ewa <ewapettke@gmail.com>
2016-12-30 18:26:47 +0100

    remove unused ads templates

--- templates/ads/ads-6109143954.php
+++
@@ -1,6 +0,0 @@
-<ins class="adsbygoogle"
-	style="display:block;width:160px;height:600px"
-	data-ad-client="ca-pub-5797055437132004"
-	data-ad-slot="6109143954">
-</ins>
-
--- templates/ads/ads-6305658572.php
+++
@@ -1,5 +0,0 @@
-<ins class="adsbygoogle"
-     data-ad-client="ca-pub-5797055437132004"
-     data-ad-slot="6305658572"
-     data-ad-format="auto">
- </ins>
