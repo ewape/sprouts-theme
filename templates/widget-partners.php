af72573212623fd26d77dcdd0a29dc1dd3c9b5f7
Ewa <ewapettke@gmail.com>
2017-01-06 18:03:40 +0100

    update partners widget

--- templates/widget-partners.php
+++ templates/widget-partners.php
@@ -1,15 +1,16 @@
 <div class="widget custom-widget widget-partners">
 	<h2 class="widget-title">Programy partnerskie</h2>
 	<div class="widget-body">
-		<a class="widget-img-link" target="_blank" href="http://www.pp.selkar.pl/aff/kielki" title="">
-			<img src="<?php echo get_template_directory_uri(); ?>/dist/images/selkar.gif" alt="Selkar">
+		<a class="widget-img-link" target="_blank" href="http://www.pp.selkar.pl/aff/kielki">
+			<img src="<?php echo get_template_directory_uri(); ?>/dist/images/selkar.gif" alt="Selkar" />
 			<span class="btn btn-accent-dark hvr-icon-pulse arrow">dołącz</span>
 		</a>
-		<a class="widget-img-link" target="_blank" href="http://www.zlotemysli.pl/kielki,1/" title="">
-			<img src="<?php echo get_template_directory_uri(); ?>/dist/images/zlotemysli-logo.png" alt="Złote myśli">
+		<a class="widget-img-link" target="_blank" href="http://pp.zlotemysli.pl/new,kielki,ZPP/">
+			<img src="<?php echo get_template_directory_uri(); ?>/dist/images/zlotemysli-logo.png" alt="Złote Myśli" />
 			<span class="btn btn-accent-dark hvr-icon-pulse arrow">dołącz</span>
 		</a>
-		<a class="widget-img-link" href="https://partnerzy.ceneo.pl/Account/Register?referrer=13274" target="_blank"><img src="https://image2.ceneo.pl/data/banners/banner_rec_3.png" class="img-responsive" />
+		<a class="widget-img-link" href="https://partnerzy.ceneo.pl/Account/Register?referrer=13274" target="_blank">
+			<img src="https://image2.ceneo.pl/data/banners/banner_rec_3.png" class="img-responsive" alt="Ceneo" />
 			<span class="btn btn-accent-dark hvr-icon-pulse arrow">dołącz</span>
 		</a>
 	</div>
