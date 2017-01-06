8e2a5c561a9e32aa31c6bab89e9c0300c3c2e284
Ewa <ewapettke@gmail.com>
2017-01-06 17:59:36 +0100

    add social links schema

--- templates/social-buttons.php
+++ templates/social-buttons.php
@@ -1,8 +1,9 @@
-<div class="social-buttons">
-	<a class="btn btn-round btn-fb" href="https://www.facebook.com/kielkiinfo-310655538217/" target="_blank" data-toggle="tooltip" data-placement="top" title="Obserwuj na Facebooku">
+<div class="social-buttons" itemscope itemtype="http://schema.org/Organization">
+	<link itemprop="url" href="http://www.kielki.info">
+	<a itemprop="sameAs" class="btn btn-round btn-fb" href="https://www.facebook.com/kielkiinfo-310655538217/" target="_blank" data-toggle="tooltip" data-placement="top" title="Obserwuj na Facebooku">
 		<i class="fa fa-facebook"></i>
 	</a>
-	<a class="btn btn-round btn-google" href="https://plus.google.com/+KielkiInfo/" target="_blank" data-toggle="tooltip" data-placement="top" title="Obserwuj na Google+">
+	<a itemprop="sameAs" class="btn btn-round btn-google" href="https://plus.google.com/+KielkiInfo/" target="_blank" data-toggle="tooltip" data-placement="top" title="Obserwuj na Google+">
 		<i class="fa fa-google-plus"></i>
 	</a>
 	<a class="btn btn-round btn-default email-me" href="#" data-toggle="tooltip" data-placement="top" title="Kontakt">
