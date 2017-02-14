bfc633f791437410599cc463fda8c381c61ed5d4
Ewa <ewapettke@gmail.com>
2017-02-14 20:38:06 +0100

    disable REST API

--- lib/extras.php
+++ lib/extras.php
@@ -13,6 +13,15 @@ remove_action( 'wp_head', 'start_post_rel_link' );
 remove_action( 'wp_head', 'index_rel_link' );
 remove_action( 'wp_head', 'adjacent_posts_rel_link' );
 remove_action( 'wp_head', 'wp_shortlink_wp_head' );
+
+// Disable REST API link tag
+remove_action('wp_head', 'rest_output_link_wp_head', 10);
+
+// Disable oEmbed Discovery Links
+remove_action('wp_head', 'wp_oembed_add_discovery_links', 10);
+
+// Disable REST API link in HTTP headers
+remove_action('template_redirect', 'rest_output_link_header', 11, 0);
 /**
  * Add <body> classes
  */
