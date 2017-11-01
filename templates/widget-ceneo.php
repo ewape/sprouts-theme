b7dc0c527b50074ff70df9011cd677bbd4b8a732
Ewa <ewapettke@gmail.com>
2017-11-01 12:48:31 +0100

    update ceneo widget

--- templates/widget-ceneo.php
+++ templates/widget-ceneo.php
@@ -1,4 +1,4 @@
 <h2>Sprawdź na ceneo.pl</h2>
-	<a href="http://www.ceneo.pl/;szukaj-kie%C5%82kownica?#pid=13274&crid=101625" target="_blank" title="">kiełkownica</a>
-	<a href="http://www.ceneo.pl/;szukaj-kie%C5%82ki?#pid=13274&crid=101625" target="_blank" title="">kiełki</a>
+	<a href="http://www.ceneo.pl/;szukaj-kie%C5%82kownica?#pid=13274&crid=140157" target="_blank" title="">kiełkownica</a>
+	<a href="http://www.ceneo.pl/;szukaj-kie%C5%82ki?#pid=13274&crid=140156" target="_blank" title="">kiełki</a>
 	<a href="http://www.ceneo.pl/;szukaj-nasiona+na%20kie%C5%82ki?#pid=13274&crid=101625" target="_blank" title="">nasiona na kiełki</a>
